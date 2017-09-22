3d849a85a7144ab6997d8cf2ffbfcb8b26de9d2d
folknor <folk@folk.wtf>
2017-09-22 13:09:55 +0200

    Bump to 1.8.0.

--- modinfo.lua
+++ modinfo.lua
@@ -4,7 +4,7 @@ return {
 	name = "Tiny Spaceport Modules",
 	tags = { "Graphics", "Utilities" },
 	picture = "thumb.png",
-	supported_version = "1.6.*",
+	supported_version = "1.8.*",
 	remote_file_id = 803452214,
 	readme = "readme.md",
 	steambb = "steam.bbcode",
